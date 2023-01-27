9858f053e2c34654d0ca825d17ec11ec301ec981
Beyzanur Özcan <Beyzanur zcan@DESKTOP-614T1MJ>
2023-01-27 10:20:07 +0300

    Add project files.

---
+++ BlogProjectMVC/Controllers/BlogController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogProjectMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        masterEntities nd = new masterEntities();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var blogDetails = nd.BLOG.ToList();
+
+            return View(blogDetails);    
+        }
+
+        public ActionResult UploadBlog ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadBlog(BLOG bg)
+        {         
+            nd.BLOG.Add(bg);
+            nd.SaveChanges();
+            ViewBag.message = "Blog basari ile eklendi.";
+            return View();
+        }
+
+        public ActionResult Food()
+        {
+            var foodArticle = nd.BLOG.Where(x => x.BCategory == "Food");
+            return View(foodArticle);
+        }
+
+        public ActionResult Sports()
+        {
+            var sportsArticle = nd.BLOG.Where(x => x.BCategory == "Sports");
+            return View(sportsArticle);
+        }
+
+        public ActionResult Movies()
+        {
+            var movieArticle = nd.BLOG.Where(x => x.BCategory == "Movies");
+            return View(movieArticle);
+        }
+
+        public ActionResult Recepies()
+        {
+            
+            return View();
+        }
+    }
+
+}
